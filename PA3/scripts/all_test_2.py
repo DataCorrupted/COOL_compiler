ce054482a10a304c85bc7ef0dd02ffc68e60c334
Jianxiong Cai <ernestme@icloud.com>
2018-05-08 00:43:39 +0800

    Add a script which only stop on encounting error

---
+++ PA3/scripts/all_test_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from cmp import compare
+
+def main():
+	os.system("./scripts/compile.sh")
+	os.system("clear")
+
+	if (len(sys.argv) == 2):
+		path = sys.argv[1]
+	else:
+		path = "test/unit/"	
+
+	for f in os.listdir(path):
+		if not (f[-3:] == ".cl"):
+			continue
+		print(f)
+		result = compare(path + f)
+		if result == 0:
+			print(f + " succeeded.")
+		else:
+			print(f + " encountered problem")
+			input()
+		print("\n")
+	os.system("rm semant")
+
+if __name__ == "__main__":
+	main();
