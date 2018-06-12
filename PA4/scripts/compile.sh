e2f6fc16cbf86df0db459d35729fada31acd56e5
Peter Rong <PeterRong96@gmail.com>
2018-06-12 10:34:04 +0800

    script changed
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- PA4/scripts/compile.sh
+++ PA4/scripts/compile.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 cd src/
-make
-mv cgen ../
 shopt -s extglob
-rm *.o 
-rm !(cgen.cc|cgen.h|Makefile|emit.h|README*|cool-tree.h|cgen_supp.cc)
-cd ..
-clear
+stderr=$(make)
+
+if (echo "$stderr" | grep failed)
+then
+  echo "Compilation failed."
+else
+	mv cgen ../
+	rm *.o 
+	rm !(cgen.cc|cgen.h|Makefile|emit.h|README*|cool-tree.h|cgen_supp.cc)
+	cd ..
+	clear
+fi
